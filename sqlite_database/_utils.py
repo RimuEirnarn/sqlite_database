d5400f3b64b8dc68e4ce7969631705d1f3e07c4b
RimuEirnarn <Rimuru720@Proton.me>
2023-05-16 16:57:11 +0700

    lines\n_utils once again failed the linting. Yes, exceeded the maximum character for a line.

--- sqlite_database/_utils.py
+++ sqlite_database/_utils.py
@@ -138,7 +138,8 @@ def future_class_var_isdefined(type_: Type[Any], future_attr: str):
     return False
 
 def test_installed():
-    """Test if project is truly installed. It was meant for docs compatibility and thus, always returns true"""
+    """Test if project is truly installed. It was meant for docs compatibility and thus,
+    always returns true"""
     return True
 
 null = NullObject()
