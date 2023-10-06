29ee2b8a0ef0daf8da50b197caf3b1b649f2b1d7
RimuEirnarn <Rimuru720@Proton.me>
2023-10-06 17:36:34 +0700

    feat(mysql-columns): Added mysql (unusable) entry

--- sqlite_database/mysqldb/__init__.py
+++ sqlite_database/mysqldb/__init__.py
@@ -3,13 +3,14 @@
 # The reason it's here is because github doesn't support forking of the same owner
 # Oh well, anyway.
 
-from typings import Any
+from typing import Any
 
 try:
     import mysql.connector # type: ignore
-except (ModuleNotFoundError):
+except ModuleNotFoundError:
     from ..errors import DependencyError
-    raise DependencyError("mysql-connector is not found, reinstall with sqlite-database[mysql]") from None
+    raise DependencyError("mysql-connector is not found, reinstall with sqlite-database[mysql]")\
+        from None
 
 from ..database import Database
 
@@ -18,4 +19,4 @@ class MySQLDatabase(Database):
 
     def __init__(self, config: dict[str, Any], **kwargs) -> None:
         mysql.connector.connect(config)
-        super().__init__("--mysql", None, **kwargs)
+        super().__init__("--mysql", **kwargs)
