6dcd7e828aa4af013b58c8642a222a50e250f993
Rimu Eirnarn <Rimuru720@proton.me>
2023-12-27 00:26:13 +0700

    Bump v.0.4.2

--- sqlite_database/__init__.py
+++ sqlite_database/__init__.py
@@ -10,6 +10,6 @@ def test_installed():
     """Is the module installed?"""
     return True
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __all__ = ["Database", "Table", "op",
            "Column", "null", 'AttrDict', 'text', 'integer', 'real', 'blob']
