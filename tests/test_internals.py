d597b59ed650e1b19edcd469412ca807829b3528
RimuEirnarn <Rimuru720@Proton.me>
2024-01-19 08:33:24 +0700

    add(test_internals): Added internal functionality tests

---
+++ tests/test_internals.py
@@ -0,0 +1,12 @@
+from sqlite_database.query_builder import extract_table
+
+SQL = "CREATE TABLE tbl (row1 text not null, row2 text not null, foreign key (row2) references tbl (row1) on delete cascade on update cascade)"
+
+
+def test_query_builder_table_01():
+    data = extract_table(SQL)
+    for col in data:
+        assert col.nullable is False
+        if col.name == "row2":
+            assert col.raw_source == "tbl/row1"
+
