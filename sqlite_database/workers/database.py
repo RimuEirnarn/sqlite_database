25f8ea56cea62abb3a4a7f7a582095e4e3c24e4f
Rimu Eirnarn <Rimuru720@proton.me>
2025-07-26 17:35:56 +0700

    Refactor DatabaseWorker connection setup to improve timeout handling and ensure proper kwargs usage

--- sqlite_database/workers/database.py
+++ sqlite_database/workers/database.py
@@ -14,11 +14,27 @@ class DatabaseWorker(Database):
         super().__init__(path, **kwargs)
 
     def _create_connection(self):
+        timeout = self._kwargs.pop("timeout", 30)
+        if not isinstance(timeout, int):
+            timeout = 30
+        # conn = connect(
+            # self._path,
+            # timeout=timeout,
+            # isolation_level=self._kwargs.pop("isolation_level", None),
+            # check_same_thread=self._kwargs.pop("check_same_thread", False)
+        # )
+        # conn.row_factory = dict_factory
         try:
             self._database = WorkerConnection(
-                self._path, worker_type=self._worker_type, **self._kwargs
+                self._path,
+                worker_type=self._worker_type,
+                timeout=timeout,
+                isolation_level=self._kwargs.pop("isolation_level", None),
+                check_same_thread=self._kwargs.pop("check_same_thread", False)
             )
             self._database.row_factory = dict_factory
+            self._database.execute("PRAGMA journal_mode=WAL;")
+            self._database.execute(f'PRAGMA busy_timeout={timeout * 1000};')
         except VersionError:
             self._database = NoopResource()
             raise
